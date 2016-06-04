120449e4a884dabff31ac1d56b9e62a8d2960303
wafrelka <wafrelka@gmail.com>
2016-06-04 21:18:35 +0900

    Implement JMA EEW parser partially (basic part of telegram)

--- src/parser/jma_format.rs
+++ src/parser/jma_format.rs
@@ -1,7 +1,6 @@
 extern crate chrono;
 
 use std::str;
-use std::ascii::AsciiExt;
 use self::chrono::*;
 use eew::*;
 
@@ -18,6 +17,19 @@ pub enum JMAFormatParseError {
 	InvalidId,
 	InvalidStatus,
 	InvalidNumber,
+	UnknownEpicenter,
+	InvalidLL,
+	InvalidDepth,
+	InvalidMagnitude,
+	InvalidMaximumIntensity,
+	InvalidEpicenterAccuracy,
+	InvalidDepthAccuracy,
+	InvalidMagnitudeAccuracy,
+	InvalidEpicenterCategory,
+	InvalidWarningStatus,
+	InvalidIntensityChange,
+	InvalidChangeReason,
+	InvalidWaveStatus,
 }
 
 fn parse_datetime(datetime_text: &[u8]) -> Option<DateTime<UTC>>
@@ -37,6 +49,32 @@ fn parse_number(number_text: &[u8]) -> Option<u32>
 	return str::from_utf8(&number_text).ok().and_then( |converted|
 		converted.parse().ok()
 	);
+}
+
+fn parse_epicenter_code(code: &[u8]) -> Option<String>
+{
+	return str::from_utf8(&code).map( |s| s.to_string()).ok();
+}
+
+fn parse_area_code(code: &[u8]) -> Option<String>
+{
+	return str::from_utf8(&code).map( |s| s.to_string()).ok();
+}
+
+fn parse_intensity(intensity_text: &[u8]) -> Option<f32>
+{
+	match intensity_text {
+		b"01" => Some(1.0),
+		b"02" => Some(2.0),
+		b"03" => Some(3.0),
+		b"04" => Some(4.0),
+		b"5-" => Some(4.75),
+		b"5+" => Some(5.25),
+		b"6-" => Some(5.75),
+		b"6+" => Some(6.25),
+		b"07" => Some(7.0),
+		_ => None
+	}
 }
 
 pub fn parse_jma_format(text: &[u8]) -> Result<EEW, JMAFormatParseError>
@@ -111,5 +149,164 @@ pub fn parse_jma_format(text: &[u8]) -> Result<EEW, JMAFormatParseError>
 		});
 	}
 
-	return Err(JMAFormatParseError::InvalidPattern);
+	let issue_pattern = match &text[0..2] {
+		b"35" => IssuePattern::IntensityOnly,
+		b"36" => IssuePattern::LowAccuracy,
+		b"37" => IssuePattern::HighAccuracy,
+		_ => return Err(JMAFormatParseError::InvalidPattern)
+	};
+
+	let epicenter_name = match parse_epicenter_code(&text[86..89]) {
+		Some(e) => e,
+		None => return Err(JMAFormatParseError::UnknownEpicenter)
+	};
+
+	let lat_value = match parse_number(&text[91..94]) {
+		Some(v) => (v as f32) / 10.0,
+		None => return Err(JMAFormatParseError::InvalidLL)
+	};
+
+	let lat = match text[90] {
+		b'N' =>  lat_value,
+		b'S' => -lat_value,
+		_ => return Err(JMAFormatParseError::InvalidLL)
+	};
+
+	let lon_value = match parse_number(&text[96..100]) {
+		Some(v) => (v as f32) / 10.0,
+		None => return Err(JMAFormatParseError::InvalidLL)
+	};
+
+	let lon = match text[95] {
+		b'E' =>  lon_value,
+		b'W' => -lon_value,
+		_ => return Err(JMAFormatParseError::InvalidLL)
+	};
+
+	let depth = match parse_number(&text[101..104]) {
+		Some(v) => Some(v as f32),
+		None => match &text[100..103] {
+			b"///" => None,
+			_ => return Err(JMAFormatParseError::InvalidDepth)
+		}
+	};
+
+	let magnitude = match parse_number(&text[105..107]) {
+		Some(v) => Some((v as f32) / 10.0),
+		None => match &text[104..106] {
+			b"///" => None,
+			_ => return Err(JMAFormatParseError::InvalidMagnitude)
+		}
+	};
+
+	let maximum_intensity = match parse_intensity(&text[108..110]) {
+		Some(v) => Some(v),
+		None => match &text[107..109] {
+			b"//" => None,
+			_ => return Err(JMAFormatParseError::InvalidMaximumIntensity)
+		}
+	};
+
+	let epicenter_accuracy = match text[113] {
+		b'1' => EpicenterAccuracy::Single,
+		b'2' => EpicenterAccuracy::Territory,
+		b'3' => EpicenterAccuracy::GridSearchLow,
+		b'4' => EpicenterAccuracy::GridSearchHigh,
+		b'5' => EpicenterAccuracy::NIEDLow,
+		b'6' => EpicenterAccuracy::NIEDHigh,
+		b'7' => EpicenterAccuracy::EPOSLow,
+		b'8' => EpicenterAccuracy::EPOSHigh,
+		b'/' => EpicenterAccuracy::Unknown,
+		_ => return Err(JMAFormatParseError::InvalidEpicenterAccuracy)
+	};
+
+	let depth_accuracy = match text[114] {
+		b'1' => DepthAccuracy::Single,
+		b'2' => DepthAccuracy::Territory,
+		b'3' => DepthAccuracy::GridSearchLow,
+		b'4' => DepthAccuracy::GridSearchHigh,
+		b'5' => DepthAccuracy::NIEDLow,
+		b'6' => DepthAccuracy::NIEDHigh,
+		b'7' => DepthAccuracy::EPOSLow,
+		b'8' => DepthAccuracy::EPOSHigh,
+		b'/' => DepthAccuracy::Unknown,
+		_ => return Err(JMAFormatParseError::InvalidDepthAccuracy)
+	};
+
+	let magnitude_accuracy = match text[115] {
+		b'2' => MagnitudeAccuracy::NIED,
+		b'3' => MagnitudeAccuracy::PWave,
+		b'4' => MagnitudeAccuracy::PSMixed,
+		b'5' => MagnitudeAccuracy::SWave,
+		b'6' => MagnitudeAccuracy::EPOS,
+		b'8' => MagnitudeAccuracy::Level,
+		b'/' => MagnitudeAccuracy::Unknown,
+		_ => return Err(JMAFormatParseError::InvalidMagnitudeAccuracy)
+	};
+
+	let epicenter_caterogy = match text[121] {
+		b'0' => EpicenterCategory::Land,
+		b'1' => EpicenterCategory::Sea,
+		b'/' => EpicenterCategory::Unknown,
+		_ => return Err(JMAFormatParseError::InvalidEpicenterCategory)
+	};
+
+	let warning_status = match text[122] {
+		b'0' => WarningStatus::Forecast,
+		b'1' => WarningStatus::Alert,
+		b'/' => WarningStatus::Unknown,
+		_ => return Err(JMAFormatParseError::InvalidWarningStatus)
+	};
+
+	let intensity_change = match text[129] {
+		b'0' => IntensityChange::Same,
+		b'1' => IntensityChange::Up,
+		b'2' => IntensityChange::Down,
+		b'/' => IntensityChange::Unknown,
+		_ => return Err(JMAFormatParseError::InvalidIntensityChange)
+	};
+
+	let change_reason = match text[130] {
+		b'0' => ChangeReason::Nothing,
+		b'1' => ChangeReason::Magnitude,
+		b'2' => ChangeReason::Epicenter,
+		b'3' => ChangeReason::Mixed,
+		b'4' => ChangeReason::Depth,
+		b'/' => ChangeReason::Unknown,
+		_ => return Err(JMAFormatParseError::InvalidChangeReason)
+	};
+
+	// TODO: parse EBI part
+
+	let area_eew = vec! {};
+
+	let detail = FullEEW {
+
+		issue_pattern: issue_pattern,
+		epicenter_name: epicenter_name.to_string(),
+		epicenter: (lat, lon),
+		depth: depth,
+		magnitude: magnitude,
+		maximum_intensity: maximum_intensity,
+		epicenter_accuracy: epicenter_accuracy,
+		depth_accuracy: depth_accuracy,
+		magnitude_accuracy: magnitude_accuracy,
+		epicenter_caterogy: epicenter_caterogy,
+		warning_status: warning_status,
+		intensity_change: intensity_change,
+		change_reason: change_reason,
+
+		area_eew: area_eew
+	};
+
+	return Ok(EEW{
+		source: source,
+		kind: kind,
+		issued_at: issued_at,
+		occurred_at: occurred_at,
+		id: id.to_string(),
+		status: status,
+		number: number,
+		detail: EEWDetail::Full(detail),
+	});
 }
--- tests/parser_test.rs
+++ tests/parser_test.rs
@@ -31,3 +31,120 @@ fn it_should_parse_cancel_eew()
 	assert!(result.is_ok());
 	assert_eq!(result.unwrap(), expected);
 }
+
+#[test]
+fn it_should_parse_normal_eew_01()
+{
+	let telegram = b"36 03 00 130804122905 C11 130804122849 \
+		ND20130804122902 NCN001 JD////////////// JN/// \
+		287 N380 E1420 010 59 04 RK11311 RT10/// RC///// \
+		9999=";
+
+	// XXX: currently we don't care about epicenter_name
+	let expected = EEW {
+		source: Source::Tokyo,
+		kind: Kind::Normal,
+		issued_at: UTC.ymd(2013, 8, 4).and_hms(3, 29, 5),
+		occurred_at: UTC.ymd(2013, 8, 4).and_hms(3, 28, 49),
+		id: "ND20130804122902".to_owned(),
+		status: Status::Normal,
+		number: 1,
+		detail: EEWDetail::Full(FullEEW {
+			issue_pattern: IssuePattern::LowAccuracy,
+			epicenter_name: "287".to_owned(),
+			epicenter: (38.0, 142.0),
+			depth: Some(10.0),
+			magnitude: Some(5.9),
+			maximum_intensity: Some(4.0),
+			epicenter_accuracy: EpicenterAccuracy::Single,
+			depth_accuracy: DepthAccuracy::Single,
+			magnitude_accuracy: MagnitudeAccuracy::PWave,
+			epicenter_caterogy: EpicenterCategory::Sea,
+			warning_status: WarningStatus::Forecast,
+			intensity_change: IntensityChange::Unknown,
+			change_reason: ChangeReason::Unknown,
+			area_eew: vec!{}
+		})
+	};
+
+	let result = parse_jma_format(telegram);
+
+	assert!(result.is_ok());
+	assert_eq!(result.unwrap(), expected);
+}
+
+#[test]
+fn it_should_parse_normal_eew_02()
+{
+	let telegram = b"37 03 00 130808165702 C11 130808165559 \
+		ND20130808165608 NCN006 JD////////////// JN/// \
+		540 N344 E1357 060 68 5+ RK33513 RT01/// RC21/// \
+		9999=";
+
+	// XXX: currently we don't care about epicenter_name
+	let expected = EEW {
+		source: Source::Tokyo,
+		kind: Kind::Normal,
+		issued_at: UTC.ymd(2013, 8, 8).and_hms(7, 57, 2),
+		occurred_at: UTC.ymd(2013, 8, 8).and_hms(7, 55, 59),
+		id: "ND20130808165608".to_owned(),
+		status: Status::Normal,
+		number: 6,
+		detail: EEWDetail::Full(FullEEW {
+			issue_pattern: IssuePattern::HighAccuracy,
+			epicenter_name: "540".to_owned(),
+			epicenter: (34.4, 135.7),
+			depth: Some(60.0),
+			magnitude: Some(6.8),
+			maximum_intensity: Some(5.25),
+			epicenter_accuracy: EpicenterAccuracy::GridSearchLow,
+			depth_accuracy: DepthAccuracy::GridSearchLow,
+			magnitude_accuracy: MagnitudeAccuracy::SWave,
+			epicenter_caterogy: EpicenterCategory::Land,
+			warning_status: WarningStatus::Alert,
+			intensity_change: IntensityChange::Down,
+			change_reason: ChangeReason::Magnitude,
+			area_eew: vec!{}
+		})
+	};
+
+	let result = parse_jma_format(telegram);
+
+	assert!(result.is_ok());
+	assert_eq!(result.unwrap(), expected);
+}
+
+#[test]
+#[ignore]
+fn it_should_parse_ebi_part()
+{
+	let telegram = b"37 03 00 130808165702 C11 130808165559 \
+		ND20130808165608 NCN006 JD////////////// JN/// \
+		540 N344 E1357 060 68 5+ RK33513 RT01/// RC21/// \
+		EBI 521 S5+5+ ////// 11 520 S5+5+ ////// 11 540 S5+5- ////// 11 \
+		511 S5+5- ////// 11 550 S5-5- ////// 11 531 S5-5- ////// 11 \
+		461 S5-5- ////// 11 462 S5-5- ////// 11 501 S5-5- ////// 11 \
+		610 S5-5- ////// 11 551 S5-04 ////// 11 535 S5-04 ////// 11 \
+		460 S5-04 ////// 11 451 S5-04 ////// 11 581 S5-04 ////// 11 \
+		532 S0404 ////// 11 600 S0404 ////// 11 601 S0404 ////// 11 \
+		500 S0404 ////// 11 510 S0404 ////// 11 401 S0404 ////// 11 \
+		530 S0404 ////// 11 432 S0404 ////// 11 450 S0404 ////// 11 \
+		580 S0404 ////// 11 431 S0404 ////// 11 611 S0404 ////// 11 \
+		560 S0404 ////// 11 400 S0404 ////// 11 443 S0404 ////// 11 \
+		630 S0404 ////// 11 562 S0404 ////// 11 631 S0404 ////// 11 \
+		391 S0404 ////// 11 620 S0404 ////// 11 563 S0404 165703 10 \
+		592 S0404 165703 10 591 S0403 ////// 11 \
+		9999=";
+
+	let expected = vec!{};
+
+	let full_result = parse_jma_format(telegram);
+	assert!(full_result.is_ok());
+
+	let result = match full_result.unwrap().detail {
+		EEWDetail::Full(v) => v.area_eew,
+		EEWDetail::Cancel => panic!(),
+	};
+
+	assert_eq!(result, expected);
+}

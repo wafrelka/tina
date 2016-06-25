3c8b0a6a5ef627231dbadbfad6ce9d634cde4355
wafrelka <wafrelka@gmail.com>
2016-06-25 22:06:08 +0900

    Remove handle field in Emitter

--- src/emitter.rs
+++ src/emitter.rs
@@ -8,7 +8,6 @@ use destination::Destination;
 
 pub struct Emitter<'a, O, F>
 	where O: 'static + Send, F: 'a + Fn(&EEW) -> Option<Box<O>> {
-	handle: thread::JoinHandle<()>,
 	tx: Sender<Box<O>>,
 	formatter: &'a F,
 }
@@ -21,7 +20,7 @@ impl<'a, O, F> Emitter<'a, O, F>
 	{
 		let (tx, rx) = channel::<Box<O>>();
 
-		let handle = thread::spawn(move || {
+		thread::spawn(move || {
 
 			loop {
 
@@ -44,7 +43,6 @@ impl<'a, O, F> Emitter<'a, O, F>
 		});
 
 		let e = Emitter {
-			handle: handle,
 			tx: tx,
 			formatter: formatter,
 		};

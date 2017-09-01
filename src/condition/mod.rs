10ea670c5bac046ec4a2537d030c56a1141ba691
wafrelka <wafrelka@gmail.com>
2017-09-02 01:15:39 +0900

    Implement DisjunctiveCondition

--- src/condition/mod.rs
+++ src/condition/mod.rs
@@ -1,6 +1,8 @@
 mod condition;
 mod constant_condition;
+mod set_condition;
 
 
 pub use self::condition::Condition;
 pub use self::constant_condition::{TRUE_CONDITION, FALSE_CONDITION};
+pub use self::set_condition::DisjunctiveCondition;

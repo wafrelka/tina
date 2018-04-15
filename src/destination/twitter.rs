226888c3e84abb54b50e29084ce5af3f54067767
wafrelka <wafrelka@gmail.com>
2018-04-15 20:51:36 +0900

    Remove direct usage of LimitedQueue in Twitter

--- src/destination/twitter.rs
+++ src/destination/twitter.rs
@@ -1,12 +1,12 @@
 use eew::EEW;
 use destination::client::TwitterClient;
 use destination::Destination;
-use collections::LimitedQueue;
+use collections::IndexedLimitedQueue;
 use translator::ja_format_eew_short;
 
 pub struct Twitter {
 	client: TwitterClient,
-	latest_tw_ids: LimitedQueue<(String, u64)>,
+	latest_tw_ids: IndexedLimitedQueue<u64>,
 	reply_enabled: bool,
 	updown_enabled: bool,
 }
@@ -17,7 +17,7 @@ impl Twitter {
 		access_key: String, access_secret: String, reply_enabled: bool, updown_enabled: bool) -> Twitter
 	{
 		let client = TwitterClient::new(consumer_key, consumer_secret, access_key, access_secret);
-		let q = LimitedQueue::new(16);
+		let q = IndexedLimitedQueue::new(16);
 
 		Twitter { client: client, latest_tw_ids: q,
 			reply_enabled: reply_enabled, updown_enabled: updown_enabled }
@@ -44,19 +44,14 @@ impl Destination for Twitter {
 		};
 
 		let prev_tw_id = match self.reply_enabled {
-			true => self.latest_tw_ids.iter().find(|x| x.0 == latest.id).map(|x| x.1),
+			true => self.latest_tw_ids.get(latest.id.as_ref()).map(|id| *id),
 			false => None
 		};
 
 		match self.client.update_status(&out, prev_tw_id) {
 
 			Ok(tw_id) => {
-
-				if prev_tw_id == None {
-					self.latest_tw_ids.push((latest.id.clone(), tw_id));
-				} else {
-					self.latest_tw_ids.iter_mut().find(|x| x.0 == latest.id).unwrap().1 = tw_id;
-				}
+				self.latest_tw_ids.upsert(latest.id.clone(), tw_id);
 			},
 
 			Err(e) => {

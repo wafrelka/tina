aafb33fa06e5693094a7dcea28c636d42855139c
wafrelka <wafrelka@gmail.com>
2018-04-20 00:01:15 +0900

    Remove unnecessary to_owned

--- tests/indexed_limited_queue.rs
+++ tests/indexed_limited_queue.rs
@@ -7,9 +7,9 @@ fn it_should_find_pushed_element()
 {
 	let mut q = IndexedLimitedQueue::new(3);
 
-	q.upsert("a".to_owned(), 1);
-	q.upsert("b".to_owned(), 2);
-	q.upsert("c".to_owned(), 3);
+	q.upsert("a", 1);
+	q.upsert("b", 2);
+	q.upsert("c", 3);
 
 	assert_eq!(q.get("a"), Some(&1));
 	assert_eq!(q.get("b"), Some(&2));
@@ -21,10 +21,10 @@ fn it_should_update_element()
 {
 	let mut q = IndexedLimitedQueue::new(3);
 
-	q.upsert("a".to_owned(), 1);
-	q.upsert("b".to_owned(), 2);
-	q.upsert("c".to_owned(), 3);
-	q.upsert("b".to_owned(), 4);
+	q.upsert("a", 1);
+	q.upsert("b", 2);
+	q.upsert("c", 3);
+	q.upsert("b", 4);
 
 	assert_eq!(q.get("a"), Some(&1));
 	assert_eq!(q.get("b"), Some(&4));
@@ -36,10 +36,10 @@ fn it_should_forget_element()
 {
 	let mut q = IndexedLimitedQueue::new(3);
 
-	q.upsert("a".to_owned(), 1);
-	q.upsert("b".to_owned(), 2);
-	q.upsert("c".to_owned(), 3);
-	q.upsert("d".to_owned(), 4);
+	q.upsert("a", 1);
+	q.upsert("b", 2);
+	q.upsert("c", 3);
+	q.upsert("d", 4);
 
 	assert_eq!(q.get("a"), None);
 	assert_eq!(q.get("b"), Some(&2));
@@ -50,13 +50,13 @@ fn it_should_create_default_element()
 {
 	let mut q = IndexedLimitedQueue::new(3);
 
-	q.upsert("a".to_owned(), 1);
-	q.upsert("b".to_owned(), 2);
-	q.upsert("c".to_owned(), 3);
+	q.upsert("a", 1);
+	q.upsert("b", 2);
+	q.upsert("c", 3);
 
-	assert_eq!(q.get_mut_default("a".to_owned()), &1);
-	assert_eq!(q.get_mut_default("d".to_owned()), &0);
-	assert_eq!(q.get("a".to_owned()), None);
+	assert_eq!(q.get_mut_default("a"), &1);
+	assert_eq!(q.get_mut_default("d"), &0);
+	assert_eq!(q.get("a"), None);
 }
 
 #[test]
@@ -64,12 +64,12 @@ fn it_should_return_old_element_when_upserting()
 {
 	let mut q = IndexedLimitedQueue::new(2);
 
-	assert_eq!(q.upsert("a".to_owned(), 100), None);
-	assert_eq!(q.upsert("a".to_owned(), 200), Some(100));
-	assert_eq!(q.upsert("a".to_owned(), 300), Some(200));
+	assert_eq!(q.upsert("a", 100), None);
+	assert_eq!(q.upsert("a", 200), Some(100));
+	assert_eq!(q.upsert("a", 300), Some(200));
 
-	q.upsert("b".to_owned(), 100);
-	q.upsert("c".to_owned(), 100);
+	q.upsert("b", 100);
+	q.upsert("c", 100);
 
-	assert_eq!(q.upsert("a".to_owned(), 100), None);
+	assert_eq!(q.upsert("a", 100), None);
 }

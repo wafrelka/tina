e47a232ffe73e4bc7346177e728609fe44df67e6
wafrelka <wafrelka@gmail.com>
2018-10-13 18:26:11 +0900

    build.rs: Handle the case when '.git' directory is removed

--- build.rs
+++ build.rs
@@ -3,6 +3,6 @@ use std::process::Command;
 fn main()
 {
 	let git_output = Command::new("git").args(&["rev-parse", "HEAD"]).output().ok();
-	let rev = git_output.and_then(|o| String::from_utf8(o.stdout).ok()).unwrap_or("<none>".to_owned());
+	let rev = git_output.filter(|o| o.status.success()).and_then(|o| String::from_utf8(o.stdout).ok()).unwrap_or("<none>".to_owned());
 	println!("cargo:rustc-env=TINA_REVISION={}", rev);
 }
